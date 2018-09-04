2072bb06af157b59cfad87d89ea994acfc180814
JonSchold <jonschold@gmail.com>
2018-09-04 11:02:12 -0700

    intro page, changing

--- newbie.js
+++ newbie.js
@@ -1,1 +1,11 @@
-document.getElementById
+window.onload = function () {
+    document.getElementById('get_started').addEventListener('click', function () {
+        document.getElementById('intro_page').style.display = 'none';
+        document.getElementById('race_page').style.display = 'block';
+    });
+
+    document.getElementById('race_page').addEventListener('click', function () {
+        this.style.display = 'none';
+        document.getElementById('intro_page').style.display = 'block';
+    });
+}
