a5238776ec04c2aca2de23b9a199fe07b5c59e6d
JonSchold <jonschold@gmail.com>
2018-09-11 10:57:50 -0700

    menu change

--- newbie.js
+++ newbie.js
@@ -28,3 +28,16 @@ function openNav() {
 function closeNav() {
     document.getElementById("sideNav").style.width = "0";
 }
+
+var character = {
+    charName = "None",
+    charRace = "None",
+    charClass = "None",
+    charAlign = "None",
+    charStr = 0,
+    charDex = 0,
+    charCon = 0,
+    charInt = 0,
+    charWis = 0,
+    charChr = 0,
+}
