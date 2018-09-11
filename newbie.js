f5219d15fe7284ba326ccd98549f0776d8dcd853
JonSchold <jonschold@gmail.com>
2018-09-11 11:15:02 -0700

    cursors, refresh

--- newbie.js
+++ newbie.js
@@ -30,14 +30,14 @@ function closeNav() {
 }
 
 var character = {
-    charName = "None",
-    charRace = "None",
-    charClass = "None",
-    charAlign = "None",
-    charStr = 0,
-    charDex = 0,
-    charCon = 0,
-    charInt = 0,
-    charWis = 0,
-    charChr = 0,
+    charName: "None",
+    charRace: "None",
+    charClass: "None",
+    charAlign: "None",
+    charStr: 0,
+    charDex: 0,
+    charCon: 0,
+    charInt: 0,
+    charWis: 0,
+    charChr: 0
 }
