bf2e692520048f4e460f8f75eead740fed3ad5d5
JonSchold <jonschold@gmail.com>
2018-09-14 15:50:06 -0700

    adding in name box and char class

--- newbie.js
+++ newbie.js
@@ -8,10 +8,69 @@ window.onload = function () {
     function introChange() {
         introPage.style.display = 'none';
         racePage.style.display = 'block';
+    };
+
+    introButton.addEventListener('click', introChange, false);
+    introButton.addEventListener('click', nameChange, false);
+};
+
+/* */
+class character {
+    constructor() {
+        this.charName = "None";
+
+        this.charRace = "None";
+        this.charClass = "None";
+        this.charAlign = "None";
+
+        this.charStr = 0;
+        this.charDex = 0;
+        this.charCon = 0;
+        this.charInt = 0;
+        this.charWis = 0;
+        this.charChr = 0;
+
+        this.HP = 0;
+
+        this.inspiration = 0;
+        this.proficiency = 0;
+
+        this.armorClass = 0;
+        this.initiative = 0;
+        this.speed = 0;
+
+        this.perception = 0;
+
+        this.acrobatics = 0;
+        this.animalHandling = 0;
+        this.arcana = 0;
+        this.athletics = 0;
+        this.deception = 0;
+        this.history = 0;
+        this.insight = 0;
+        this.intimidation = 0;
+        this.investigation = 0;
+        this.medicine = 0;
+        this.nature = 0;
+        this.perception = 0;
+        this.performance = 0;
+        this.persuasion = 0;
+        this.religion = 0;
+        this.sleightOfHand = 0;
+        this.stealth = 0;
+        this.survival = 0;
+
+        this.weapons = ["none"];
+        this.spells = ["none"];
+
+        this.languages = ["none"];
+        this.equipment = ["none"];
+
+        this.feats = ["none"];
     }
 
 
-    introPage.addEventListener('click', introChange, false);
+
 }
 
 /* Change the picture depending on race selection */
@@ -29,15 +88,10 @@ function closeNav() {
     document.getElementById("sideNav").style.width = "0";
 }
 
-var character = {
-    charName: "None",
-    charRace: "None",
-    charClass: "None",
-    charAlign: "None",
-    charStr: 0,
-    charDex: 0,
-    charCon: 0,
-    charInt: 0,
-    charWis: 0,
-    charChr: 0
+/* Takes the user's name input and sets the objects name field */
+function nameChange() {
+    newChar.charName = document.getElementById('charNameInput').value;
 }
+
+/* Create the character for the user*/
+const newChar = new character;
